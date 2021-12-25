9995750b8c7f39c94b2a2842c0ca6e543fb0f3a3
Casey Watts <casey.s.watts@gmail.com>
2021-12-25 15:44:34 -0500

    update abcjs to v6.0.0-beta.356

--- index.js
+++ index.js
@@ -11,158 +11,187 @@ function el(name) {
 
 
 // Transposing UI
-  const TRANSPOSE_MAP = {
-    "Ab": -4,
-    "A": -3,
-    "Bb": -2,
-    "B": -1,
-    "C": 0,
-    "C#": 1,
-    "D": 2,
-    "Eb": 3,
-    "E": 4,
-    "F": 5,
-    "F#": 6,
-    "G": -5
+const TRANSPOSE_MAP = {
+  "Ab": -4,
+  "A": -3,
+  "Bb": -2,
+  "B": -1,
+  "C": 0,
+  "C#": 1,
+  "D": 2,
+  "Eb": 3,
+  "E": 4,
+  "F": 5,
+  "F#": 6,
+  "G": -5
+};
+
+const OCTAVE_MAP = {
+  "+3": 36,
+  "+2": 24,
+  "+1": 12,
+  "same": 0,
+  "-1": -12,
+  "-2": -24,
+  "-3": -36
+}
+
+function setupInstrumentKeyDropdown() {
+  // populate dropdown options
+  Object.keys(TRANSPOSE_MAP).forEach(key => {
+    value = TRANSPOSE_MAP[key];
+    var option = document.createElement('option')
+    option.text = key
+    option.value = value
+    el('instrumentKeyDropdown').appendChild(option);
+  })
+
+  // set default from LocalStorage
+  var storedValue = window.localStorage.getItem('instrumentKey')
+  if (storedValue) {
+    el('instrumentKeyDropdown').value = storedValue;
+  } else {
+    el('instrumentKeyDropdown').value = 0;
+  }
+}
+
+function setupOctaveDropdown() {
+  // populate dropdown options
+  Object.keys(OCTAVE_MAP).forEach(key => {
+    value = OCTAVE_MAP[key];
+    var option = document.createElement('option')
+    option.text = key
+    option.value = value
+    el('octaveDropdown').appendChild(option);
+  })
+
+  // set default from LocalStorage
+  var storedValue = window.localStorage.getItem('octaveChange')
+  if (storedValue) {
+    el('octaveDropdown').value = storedValue;
+  } else {
+    el('octaveDropdown').value = 0;
+  }
+}
+
+function instrumentKeyChanged() {
+  window.localStorage.setItem('instrumentKey', el('instrumentKeyDropdown').value);
+  renderCurrentTune();
+}
+
+function octaveChanged() {
+  window.localStorage.setItem('octaveChange', el('octaveDropdown').value);
+  renderCurrentTune();
+}
+
+function transposeAmount() {
+  var instrumentKeyTransposeAmount = parseInt(el('instrumentKeyDropdown').value);
+  var octaveChangeTransposeAmount = parseInt(el('octaveDropdown').value);
+  return instrumentKeyTransposeAmount + octaveChangeTransposeAmount;
+}
+
+function insertAudio(destinationSelector, abc, abcOptions, audioParams) {
+  if (ABCJS.synth.supportsAudio()) {
+    var synthControl = new ABCJS.synth.SynthController();
+    synthControl.load(destinationSelector,
+      null,
+      {
+        displayLoop: true,
+        displayRestart: true,
+        displayPlay: true,
+        displayProgress: true,
+        displayWarp: true
+      }
+    );
+
+    var visualObj = ABCJS.renderAbc("paper",
+      abc, abcOptions);
+    var createSynth = new ABCJS.synth.CreateSynth();
+    createSynth.init({ visualObj: visualObj[0] }).then(function () {
+      synthControl.setTune(visualObj[0], false, audioParams).then(function () {
+        console.log("Audio successfully loaded.")
+      }).catch(function (error) {
+        console.warn("Audio problem:", error);
+      });
+    }).catch(function (error) {
+      console.warn("Audio problem:", error);
+    });
+  } else {
+    document.querySelector("#audio").innerHTML =
+      "Audio is not supported in this browser.";
+  }
+}
+
+function renderCurrentTune() {
+  let abc = currentTune;
+  let abcOptions = {
+    visualTranspose: transposeAmount(),
+    responsive: "resize"
   };
 
-  const OCTAVE_MAP = {
-    "+3": 36,
-    "+2": 24,
-    "+1": 12,
-    "same": 0,
-    "-1": -12,
-    "-2": -24,
-    "-3": -36
-  }
+  //insertAudio also inserts this I think - might be good to not have that update this 3x
+  ABCJS.renderAbc('paper', abc, abcOptions);
 
-  function setupInstrumentKeyDropdown() {
-    // populate dropdown options
-    Object.keys(TRANSPOSE_MAP).forEach(key => {
-      value = TRANSPOSE_MAP[key];
-      var option = document.createElement('option')
-      option.text = key
-      option.value = value
-      el('instrumentKeyDropdown').appendChild(option);
-    })
-
-    // set default from LocalStorage
-    var storedValue = window.localStorage.getItem('instrumentKey')
-    if (storedValue) {
-      el('instrumentKeyDropdown').value = storedValue;
-    } else {
-      el('instrumentKeyDropdown').value = 0;
-    }
-  }
-
-  function setupOctaveDropdown() {
-    // populate dropdown options
-    Object.keys(OCTAVE_MAP).forEach(key => {
-      value = OCTAVE_MAP[key];
-      var option = document.createElement('option')
-      option.text = key
-      option.value = value
-      el('octaveDropdown').appendChild(option);
-    })
-
-    // set default from LocalStorage
-    var storedValue = window.localStorage.getItem('octaveChange')
-    if (storedValue) {
-      el('octaveDropdown').value = storedValue;
-    } else {
-      el('octaveDropdown').value = 0;
-    }
-  }
-
-  function instrumentKeyChanged(){
-    window.localStorage.setItem('instrumentKey', el('instrumentKeyDropdown').value);
-    renderCurrentTune();
-  }
-
-  function octaveChanged(){
-    window.localStorage.setItem('octaveChange', el('octaveDropdown').value);
-    renderCurrentTune();
-  }
-
-  function transposeAmount() {
-    var instrumentKeyTransposeAmount = parseInt(el('instrumentKeyDropdown').value);
-    var octaveChangeTransposeAmount = parseInt(el('octaveDropdown').value);
-    return instrumentKeyTransposeAmount + octaveChangeTransposeAmount;
-  }
-
-  function renderCurrentTune() {
-    ABCJS.renderAbc('paper', currentTune, {
-      visualTranspose: transposeAmount(),
-      responsive: "resize"
-    });
-    ABCJS.renderMidi('midi-full', currentTune, {
-      inlineControls: { loopToggle: true }
-    });
-    ABCJS.renderMidi('midi-melody', currentTune, {
-      inlineControls: { loopToggle: true },
-      chordsOff: true
-    });
-    ABCJS.renderMidi('midi-accompaniment', currentTune, {
-      inlineControls: { loopToggle: true },
-      voicesOff: true
-    });
-  }
+  insertAudio("#midi-full", abc, abcOptions, {});
+  insertAudio("#midi-melody", abc, abcOptions, { chordsOff: true });
+  insertAudio("#midi-accompaniment", abc, abcOptions, { voicesOff: true });
+}
 
 
 // Tune Storage in URL
-  function updateUrlToCurrentTune() {
-    window.location.search = `?tune=${encodeURIComponent(currentTune)}`;
-  }
+function updateUrlToCurrentTune() {
+  window.location.search = `?tune=${encodeURIComponent(currentTune)}`;
+}
 
-  function renderFromURL() {
-    const params = (new URL(document.location)).searchParams;
-    const tuneString = params.get('tune');
-    
-    if (tuneString) {
-      currentTune = tuneString
-      renderCurrentTune();
-      toast("tune read from URL", "goodColor");
-    } else {
-      renderExample();
-      toast("showing example tune", "goodColor");
-    }
+function renderFromURL() {
+  const params = (new URL(document.location)).searchParams;
+  const tuneString = params.get('tune');
+
+  if (tuneString) {
+    currentTune = tuneString
+    renderCurrentTune();
+    toast("tune read from URL", "goodColor");
+  } else {
+    renderExample();
+    toast("showing example tune", "goodColor");
   }
+}
 
 
 // clipboard
-  function renderFromClipboard() {
-    navigator.clipboard.readText().then((text) => {
-      currentTune = text;
-      renderCurrentTune()
-      updateUrlToCurrentTune();
-      toast("tune read from clipboard, URL updated", "goodColor");
-    }).catch(() => {
-      renderExample()
-      toast("no tune in clipboard, showing example tune", "badColor");
-    })
-  }
+function renderFromClipboard() {
+  navigator.clipboard.readText().then((text) => {
+    currentTune = text;
+    renderCurrentTune()
+    updateUrlToCurrentTune();
+    toast("tune read from clipboard, URL updated", "goodColor");
+  }).catch(() => {
+    renderExample()
+    toast("no tune in clipboard, showing example tune", "badColor");
+  })
+}
 
-  function copyToClipboard() {
-    navigator.clipboard.writeText(currentTune).then(function() {
-      toast("abc copied to clipboard", "goodColor");
-    }).catch(() => {
-      toast("could not copy abc to clipboard", "badColor");
-    })
-  }
+function copyToClipboard() {
+  navigator.clipboard.writeText(currentTune).then(function () {
+    toast("abc copied to clipboard", "goodColor");
+  }).catch(() => {
+    toast("could not copy abc to clipboard", "badColor");
+  })
+}
 
 
 
 // toast
-  function toast(text, className) {
-    el('flash').textContent = text;
-    el('flash').classList.remove('display-none');
-    el('flash').classList.add("quickFlash")
-    el('flash').classList.add(className)
+function toast(text, className) {
+  el('flash').textContent = text;
+  el('flash').classList.remove('display-none');
+  el('flash').classList.add("quickFlash")
+  el('flash').classList.add(className)
 
-    setTimeout(function(){
-      el('flash').classList.add('display-none');
-    }, 3000);
-  }
+  setTimeout(function () {
+    el('flash').classList.add('display-none');
+  }, 3000);
+}
 
 
 // exampleTune
@@ -187,11 +216,11 @@ K:G
 
 
 // HTTPS
-  function redirectHTTPS() {
-    if ((window.location.host !== "localhost:3000") && (window.location.protocol != 'https:')) {
-      window.location.protocol = 'https';
-    }
+function redirectHTTPS() {
+  if ((window.location.host !== "localhost:3000") && (window.location.protocol != 'https:')) {
+    window.location.protocol = 'https';
   }
+}
 
 // // MIDI Configuration
 //   const GRAND_PIANO_SOUND_FONT_URL = '/';
@@ -201,13 +230,13 @@ K:G
 //   }
 
 // Page Setup
-  window.onload = function(event) {
-    // renderFromClipboard();
-    redirectHTTPS();
+window.onload = function (event) {
+  // renderFromClipboard();
+  redirectHTTPS();
 
-    setupInstrumentKeyDropdown();
-    setupOctaveDropdown();
+  setupInstrumentKeyDropdown();
+  setupOctaveDropdown();
 
-    // configureMIDI();
-    renderFromURL();
-  }
+  // configureMIDI();
+  renderFromURL();
+}
